dada09f71d0a41b7592adadd75ce30a569565552
Raiyan (Shadow) <82103149+shadow855@users.noreply.github.com>
2024-06-03 14:37:27 +0530

    Update SideDrawer.js

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -142,8 +142,8 @@ function SideDrawer() {
           </Button>
         </Tooltip>
         <Text fontSize="2xl" fontFamily="Work sans">
-          Talk-A-Tive
-        </Text>
+  Shadow-Chat-App
+  </Text>
         <div>
           <Menu>
             <MenuButton p={1}>
